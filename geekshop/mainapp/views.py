9a83c2d485aade10d48e13b91bb524671005b047
german-dev25 <german.apx@yandex.ru>
2022-01-24 17:48:32 +0300

    Выполнил ДЗ-2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,44 @@
 from django.shortcuts import render
+import json
 
+MENU_LINKS = [
+    {'url': 'main', 'name': 'домой'},
+    {'url': 'products', 'name': 'продукты'},
+    {'url': 'contact', 'name': 'контакты'},
+]
+
+# PRODUCTS_CATEGORY = ['все', 'дом', 'офис', 'модерн', 'классика', ]
+# PRODUCTS_CATEGORY_HREF = ['#', '#', '#', '#', '#', ]
+
+PRODUCTS_LINKS = [
+    {'href': '#', 'name': 'все'},
+    {'href': '#', 'name': 'дом'},
+    {'href': '#', 'name': 'офис'},
+    {'href': '#', 'name': 'модерн'},
+    {'href': '#', 'name': 'классика'},
+]
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={
+        'title': 'Главная',
+        'menu_links': MENU_LINKS,
+    })
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    with open('products.json', 'r', encoding='UTF-8') as file:
+        products = json.load(file)
+
+    return render(request, 'mainapp/products.html', context={
+        'title': 'Продукты',
+        'menu_links': MENU_LINKS,
+        'products': products,
+        'products_menu': PRODUCTS_LINKS,
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
-
+    return render(request, 'mainapp/contact.html', context={
+        'title': 'Контакты',
+        'menu_links': MENU_LINKS,
+    })
