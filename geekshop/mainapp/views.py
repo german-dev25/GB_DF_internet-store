65de51424255506d695b8d39e60749a1769cd1ab
german-dev25 <german.apx@yandex.ru>
2022-01-27 17:21:12 +0300

    Выполнил ДЗ-3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,41 +1,37 @@
 from django.shortcuts import render
-import json
+from .models import Product, ProductCategory
 
 MENU_LINKS = [
     {'url': 'main', 'name': 'домой'},
-    {'url': 'products', 'name': 'продукты'},
+    {'url': 'mainapp:products', 'name': 'продукты'},
     {'url': 'contact', 'name': 'контакты'},
 ]
 
-# PRODUCTS_CATEGORY = ['все', 'дом', 'офис', 'модерн', 'классика', ]
-# PRODUCTS_CATEGORY_HREF = ['#', '#', '#', '#', '#', ]
-
-PRODUCTS_LINKS = [
-    {'href': '#', 'name': 'все'},
-    {'href': '#', 'name': 'дом'},
-    {'href': '#', 'name': 'офис'},
-    {'href': '#', 'name': 'модерн'},
-    {'href': '#', 'name': 'классика'},
-]
 
 def index(request):
+    products = Product.objects.all()[:4]
+
     return render(request, 'mainapp/index.html', context={
         'title': 'Главная',
         'menu_links': MENU_LINKS,
+        'products': products,
     })
 
 
 def products(request):
-    with open('products.json', 'r', encoding='UTF-8') as file:
-        products = json.load(file)
-
+    # with open('products.json', 'r', encoding='UTF-8') as file:
+    #     products = json.load(file)
+    categories = ProductCategory.objects.all()
     return render(request, 'mainapp/products.html', context={
         'title': 'Продукты',
         'menu_links': MENU_LINKS,
-        'products': products,
-        'products_menu': PRODUCTS_LINKS,
+        'products': [],
+        'categories': categories
     })
 
+
+def category(request, pk):
+    return products(request)
 
 def contact(request):
     return render(request, 'mainapp/contact.html', context={
